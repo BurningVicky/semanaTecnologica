8d99f51be333bb929cd21a142e9db6de50022beb
Vicky <98425423+BurningVicky@users.noreply.github.com>
2025-09-18 16:55:39 -0300

    Update 0.8a
    Bug fixes on gh pages, ready for deploy.

--- src/components/Code-component-3-19.tsx
+++ src/components/Code-component-3-19.tsx
@@ -60,7 +60,7 @@ export function Hero() {
           <div className="relative">
             <div className="relative z-10">
               <ImageWithFallback 
-                src="public/img/animeCharMetaVR.jpg"
+                src="https://images.unsplash.com/photo-1715866558475-d2543c67a840?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHx8fDE3NTc3MDI3NzZ8MA&ixlib=rb-4.1.0&q=80&w=1080"
                 alt="Pessoa usando VR"
                 className="w-full h-auto rounded-lg shadow-2xl"
               />
