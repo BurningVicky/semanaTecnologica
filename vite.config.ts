80c13de7eaecd499cbfc9c72b49ca2135d7361a9
Vicky <98425423+BurningVicky@users.noreply.github.com>
2025-09-18 20:19:11 -0300

    vite fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/semanaTecnologica/',
+    base: '/semanaTecnologica',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
